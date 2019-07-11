31b06200c28138f289d767ecc0fd6dddc8c02bfb
Jakub <k_jakub@icloud.com>
2019-07-11 16:45:14 +0200

    Added exception handling

--- src/Enviroment.py
+++ src/Enviroment.py
@@ -1,15 +1,15 @@
 from datetime import timedelta, datetime
-from gpiozero import DigitalOutputDevice
+from gpiozero import DigitalOutputDevice, GPIOZeroError
 from sched import scheduler
 from configparser import ConfigParser
 from helpers import strToTimedelta
 from enum import Enum, auto
 
+CONFIGFILE_DEFAULT_PATH = 'enviroment.cfg'
 
-CONFIGFILE_DEFAULT_PATH='enviroment.cfg'
 
 class SCHED_PRIORITY_TABLE(Enum):
-    #priority low to high
+    # priority low to high
     waterOn = auto()
     isItRightToWaterNow = auto()
     SCHED_STOP = auto()
@@ -28,41 +28,61 @@ DEFAULT_INTERVAL = 300
 
 class Plant:
     plantName = ''
-    gpioPinNumber: int
+    gpioPinNumber: str
     wateringDuration: timedelta
     wateringInterval: timedelta
     lastTimeWatered: datetime.min
     pumpSwitch: DigitalOutputDevice
-    plantEnviroment: Enviroment
+    plantEnviroment: None #TODO typing - Enviroment
+    global DEFAULT_INTERVAL
 
-
-    def __init__ (self, plantName: str, gpioPinNumber: int, wateringDuration: timedelta, wateringInterval: timedelta, enviroment: Enviroment):
+    def __init__(self, plantName: str, gpioPinNumber: str, wateringDuration: timedelta, wateringInterval: timedelta,
+                 enviroment: None):
         self.plantName = plantName
         self.wateringDuration = wateringDuration
         self.wateringInterval = wateringInterval
         self.gpioPinNumber = gpioPinNumber
         self.plantEnviroment = enviroment
         try:
-            self.pumpSwitch = DigitalOutputDevice("BOARD" + str(gpioPinNumber), active_high=False, initial_value=True)
+            self.pumpSwitch = DigitalOutputDevice(gpioPinNumber, active_high=False, initial_value=True)
         except:
-            raise Exception("Couldn't set up gpio pin")
+            raise Exception("Error: Couldn't set up gpio pin. Quitting!")
 
     def waterOff(self):
-        self.pumpSwitch.off()
-        self.lastTimeWatered = datetime.now()
-        self.plantEnviroment.envScheduler.enter(delay= self.wateringInterval, priority= SCHED_PRIORITY_TABLE.isItRigthToWaterNow, action= self.isItRigthToWaterNow())
+        try:
+            self.pumpSwitch.off()
+            self.lastTimeWatered = datetime.now()
+            self.plantEnviroment.updateConfigSection(sectionName= self.plantName, option= 'lastTimeWatered', val= self.lastTimeWatered)
+            self.plantEnviroment.envScheduler.enter(delay= self.wateringInterval,
+                                                    priority= SCHED_PRIORITY_TABLE.isItRigthToWaterNow,
+                                                    action= self.isItRigthToWaterNow())
+        except GPIOZeroError:
+            self.plantEnviroment.envScheduler.enter(delay= 0, priority= SCHED_PRIORITY_TABLE.SCHED_STOP, action= self.plantEnviroment.killScheduler())
+            raise Exception('ERROR: GPIO error. Quitting!')
+        except Exception as err:
+            self.plantEnviroment.envScheduler.enter(delay= 0, priority= SCHED_PRIORITY_TABLE.SCHED_STOP, action= self.plantEnviroment.killScheduler())
+            raise err
 
-    def waterOn (self):
-        self.pumpSwitch.on()
-        self.plantEnviroment.envScheduler.enter(delay= self.wateringDuration, priority= SCHED_PRIORITY_TABLE.waterOff, action= self.waterOff())
 
-    def isItRigthToWaterNow (self):
+    def waterOn(self):
+        try:
+            self.pumpSwitch.on()
+            self.plantEnviroment.envScheduler.enter(delay= self.wateringDuration, priority= SCHED_PRIORITY_TABLE.waterOff,
+                                                    action= self.waterOff())
+        except GPIOZeroError:
+            self.plantEnviroment.envScheduler.enter(delay= 0, priority= SCHED_PRIORITY_TABLE.SCHED_STOP, action= self.plantEnviroment.killScheduler())
+            raise Exception('ERROR: GPIO error. Quitting!')
+
+
+    def isItRigthToWaterNow(self):
         if datetime.now() - self.lastTimeWatered >= self.wateringInterval:
-            #sched water
-            self.plantEnviroment.envScheduler.enter(delay= 0, priority= SCHED_PRIORITY_TABLE.waterOn, action= self.waterOn())
+            # sched water
+            self.plantEnviroment.envScheduler.enter(delay=0, priority=SCHED_PRIORITY_TABLE.waterOn,
+                                                    action=self.waterOn())
         else:
-            self.plantEnviroment.envScheduler.enter(delay= DEFAULT_INTERVAL, priority= SCHED_PRIORITY_TABLE.isItRightToWaterNow, action= self.isItRigthToWaterNow())
-
+            self.plantEnviroment.envScheduler.enter(delay=DEFAULT_INTERVAL,
+                                                    priority=SCHED_PRIORITY_TABLE.isItRightToWaterNow,
+                                                    action=self.isItRigthToWaterNow())
 
 
 
@@ -72,57 +92,71 @@ class Enviroment:
     envScheduler = scheduler()
     envSchedulerState = SCHED_STATE.UNSET
     eventsOutOfQueue = []
+    cfg_path: str
 
-    def __init__(self, cfg_path: str):
-        self.readConfig(cfg_path)
+    def __init__(self, cfg_path = CONFIGFILE_DEFAULT_PATH):
+        self.cfg_path = cfg_path
+
+        self.readConfig()
         self.start()
 
-
-
-    def readConfig(self, cfg_path= CONFIGFILE_DEFAULT_PATH):
+    def readConfig(self):
         config = ConfigParser()
-        config.read(filenames= cfg_path)
+        if not config.read(filenames= self.cfg_path):
+            raise Exception('Error: enviroment.cfg file not found. Quitting!')
 
-        #read global section
+        # read global section
         global globalConfigSection
         globalConfigSection = config['GLOBAL']
 
-        DEFAULT_INTERVAL = globalConfigSection['DEFAULT_INTERVAL']
+        global DEFAULT_INTERVAL
+        try:
+            DEFAULT_INTERVAL = int(globalConfigSection['DEFAULT_INTERVAL'])
+        except KeyError:
+            print('Warning: DEFAULT_INTERVAL unset; setting to 300s')
+        except ValueError:
+            raise Exception('Error: DEFAULT_INTERVAL value is not a number. Quitting!')
 
-        #read plants
+        # read plants
         for section in config:
             if section != 'GLOBAL':
+                sectionName = section.name
+                try:
+                    params = {}
 
-                newPlant: Plant
+                    params['plantName'] = str(section.name)
+                    params['wateringDuration'] = timedelta(seconds= int(section['wateringDuration']))
+                    params['wateringInterval'] = strToTimedelta.parse(time_str= section['wateringInterval'])
+                    params['lastTimeWatered'] = datetime.strptime(date_string=section['lastTimeWatered'],
+                                                                  format='%Y-%m-%d %H:%M%:%S')
+                    params['gpioPinNumber'] = str(section['gpioPinNumber'])
 
-                params = {}
+                    newPlant = Plant(**params)
 
-                uniqueName = str(section.name)
-                params['plantName'] = str(section['plantName'])
-                params['wateringDuration'] = timedelta(seconds= section['wateringDuration'])
-                params['wateringInterval'] = strToTimedelta.parse(time_str= section['wateringInterval'])
-                params['lastTimeWatered'] = datetime.strptime(date_string= section['lastTimeWatered'], format= '%Y-%m-%d %H:%M%:%S')
-                params['gpioPinNumber'] = int(section['gpioPinNumber'])
-
-                newPlant = Plant(**params)
-
-                self.plants.append(newPlant)
+                    self.plants.append(newPlant)
+                except KeyError as err:
+                    print('Warning: enviroment.cfg: Failed to read ' + sectionName + 'section - option not found ' + str(err))
+                except ValueError:
+                    print('Warning: enviroment.cfg: Failed to read ' + sectionName + 'section - wrong argument value' )
+                except Exception as err:
+                    print('Warning: enviroment.cfg: Failed to read ' + sectionName + 'section' + str(err))
 
 
-    def scheduleMonitoring(self):
+
+    def schedule_monitoring(self):
         for plant in self.plants:
             plant.isItRigthToWaterNow()
         self.envSchedulerState = SCHED_STATE.STOPPED
 
     def start(self):
         if self.envSchedulerState == SCHED_STATE.STOPPED:
-            self.envScheduler.run()
+            self.__resumeScheduler()
 
     def stop(self):
-        if self.envSchedulerState in [SCHED_STATE.RUNNING, SCHED_STATE.PAUSED]:
-            self.envScheduler.enter(delay= 0, priority= SCHED_PRIORITY_TABLE.SCHED_STOP, action= self.__stopScheduler())
+        if self.envSchedulerState == SCHED_STATE.RUNNING:
+            self.envSchedulerState = SCHED_STATE.PAUSED
+            self.envScheduler.enter(delay=0, priority=SCHED_PRIORITY_TABLE.SCHED_STOP, action=self.__stopScheduler())
 
-    #in development
     def __stopScheduler(self):
         for event in self.envScheduler.queue:
             if event.action == Plant.waterOff:
@@ -130,12 +164,33 @@ class Enviroment:
             self.eventsOutOfQueue.append(event)
             self.envScheduler.cancel(event)
 
-    #in development
+        self.envSchedulerState = SCHED_STATE.STOPPED
+
+    def killScheduler(self):
+        for event in self.envScheduler.queue:
+            if event.action == Plant.waterOff:
+                event.action()
+            self.envScheduler.cancel(event)
+
     def __resumeScheduler(self):
         for event in self.eventsOutOfQueue:
             self.envScheduler.enter(**event)
 
         self.eventsOutOfQueue.clear()
+        self.envSchedulerState = SCHED_STATE.RUNNING
+        self.envScheduler.run()
 
+    def updateConfigSection(self, sectionName: str, option: str, val):
+        config = ConfigParser()
 
+        if not config.read(filenames= self.cfg_path):
+            raise FileNotFoundError('Error: Enviroment config file not found')
+
+        config[sectionName][option] = str(val)
+        try:
+            cfg_file = open(file= self.cfg_path, mode= 'w')
+            config.write(fp= cfg_file)
+        except:
+
+            raise Exception('Error: Couldn\'t write config to file')
 
--- src/helpers/strToTimedelta.py
+++ src/helpers/strToTimedelta.py
@@ -8,10 +8,10 @@ regex = re.compile(r'((?P<days>\d+?)D)?((?P<hours>\d+?):)?((?P<minutes>\d+?):)?(
 def parse(time_str: str):
     parts = regex.match(time_str)
     if not parts:
-        return
+        raise ValueError('String does not match proper pattern')
     parts = parts.groupdict()
     time_params = {}
-    for (name, param) in parts.iteritems():
+    for (name, param) in parts:
         if param:
             time_params[name] = int(param)
     return timedelta(**time_params)
