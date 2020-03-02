e66542b3ffaa0b153328f9b0ef0737d2531f849e
Jakub <k_jakub@icloud.com>
2020-03-02 22:28:20 +0100

    Added requirements

--- src/PlantStation/Configure.py
+++ src/PlantStation/Configure.py
@@ -15,8 +15,8 @@ from PlantStation.Plant import Plant
 from PlantStation.helpers.format_validators import parse_time
 
 PI_GPIO = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
-WORKDIR = '/etc/plantstation'
-GLOBAL_CFG_PATH = Path('/etc/')  # TODO pathlib
+WORKDIR = Path('/etc/plantstation')
+GLOBAL_CFG_PATH = Path('/etc/')
 USER_CFG_PATH = Path('~/.config/').expanduser()
 CFG_FILENAME = Path('plantstation.cfg')
 LOGFILE_PATH = Path('/var/log/plantstation.log')
